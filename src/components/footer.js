5888e29eebf0362e9569db7bc80235ebac391c55
Max Rasp <maksym.rasporskyi@barzahstudio.com>
2023-08-12 10:49:06 +0300

    top

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
             <p className="text-sm md:text-lg leading-8 text-gray-600">Copyright@AnastasiiaPolubynska</p>
             <p className="hidden md:block text-sm md:text-lg text-gray-600">^_^</p>
             <a href="#" className="hidden md:block text-sm md:text-lg leading-8 text-gray-600">Back to top</a>
+            <a href="#" className="sm:hidden text-sm md:text-lg leading-8 text-gray-600">Top ↑</a>
         </div>
     )
 }
