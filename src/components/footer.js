b555391a0be675f1001dd25a0506368abd53eab7
Max Rasp <maksym.rasporskyi@barzahstudio.com>
2023-08-12 10:50:38 +0300

    back to top

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
         <div className="flex justify-between pt-24 pb-12">
             <p className="text-sm md:text-lg leading-8 text-gray-600">Copyright@AnastasiiaPolubynska</p>
             <p className="hidden md:block text-sm md:text-lg text-gray-600">^_^</p>
-            <a href="#" className="hidden md:block text-sm md:text-lg leading-8 text-gray-600">Back to top</a>
+            <a href="#" className="hidden sm:block text-sm md:text-lg leading-8 text-gray-600">Back to top</a>
             <a href="#" className="sm:hidden text-sm md:text-lg leading-8 text-gray-600">Top ↑</a>
         </div>
     )
