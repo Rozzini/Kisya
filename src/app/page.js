a51879aada46644f4c8f52a8d85be6260f643f68
Max Rasp <maksym.rasporskyi@barzahstudio.com>
2023-08-20 17:07:24 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Head from "next/head";
 import Header    from "../components/header"
 import Hero      from "../components/hero"
 import Portfolio from "../components/portfolio"
@@ -8,6 +8,9 @@ import Footer from "../components/footer"
 export default function Home() {
   return (
     <main className="bg-white px-4 md:px-12">
+      <Head>
+        <meta property="og:image" content="/home/SocialMedia.png" />
+      </Head>
       <Header />
       <Hero />
       <Portfolio />
