9cdb386039ca98e2de1d8ccb5db2f8d0a56b5f2f
Max Rasp <maksym.rasporskyi@barzahstudio.com>
2023-08-20 16:55:56 +0300

    title +

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'AP',
+  title: 'UI/UX DESIGNER',
   description: '^_^',
 }
 
