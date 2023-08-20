9ac452702ca88b31c4a07dedffb7ca811b8a1db3
Max Rasp <maksym.rasporskyi@barzahstudio.com>
2023-08-20 17:12:23 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,11 @@ export default function Home() {
     <main className="bg-white px-4 md:px-12">
       <Head>
         <meta property="og:image" content="/home/SocialMedia.png" />
+        <meta property="og:title" content="My Next.js App" />
+        <meta
+          property="og:description"
+          content="This is a sample app for OGP settings"
+        />
       </Head>
       <Header />
       <Hero />
