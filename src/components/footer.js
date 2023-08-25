b30d22272a14e5e2cc113e87cbc697d9109ea9ca
Max Rasp <maksym.rasporskyi@barzahstudio.com>
2023-08-25 12:18:20 +0300

    anaRasp

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="flex justify-between pb-12">
-            <p className="text-sm md:text-lg leading-8 text-gray-600">Copyright@AnastasiiaPolubynska</p>
+            <p className="text-sm md:text-lg leading-8 text-gray-600">Copyright@AnaRasp</p>
             <p className="hidden md:block text-sm md:text-lg text-gray-600">^_^</p>
             <a href="#" className="hidden sm:block text-sm md:text-lg leading-8 text-gray-600">Back to top</a>
             <a href="#" className="sm:hidden text-sm md:text-lg leading-8 text-gray-600">Top ↑</a>
