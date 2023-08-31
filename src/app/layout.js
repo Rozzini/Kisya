b6e19986098bc090998fd4a52be882be0cb7de8f
Max Rasp <maksym.rasporskyi@barzahstudio.com>
2023-08-31 21:24:52 +0300

    analitics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
   return (
     <html class="scroll-smooth" lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   )
 }
